c5f2d68cd28613e09c5df6967440e1efc124d0ea
gabopr <gisantiago@live.com>
2015-06-04 02:26:45 +0000

    End of the book exercises

---
+++ End_Of_Book.rb
@@ -0,0 +1,62 @@
+# Exercise 1: use each method of Array to iterate...
+
+a = [1,2,3,4,5,6,7,8,9,10]
+
+a.each {|n| puts n} 
+
+
+# Exercise 2: print only values greater than 5
+
+a.each do |n|
+    if n > 5
+        puts n
+    end
+end
+
+# Exercise 3: print only odd values into a new array
+
+# a.select do |n|
+#     if n.odd?
+#         p n
+#     end
+# end
+
+
+new_a = a.select { |n| n.odd? }
+        p new_a
+        
+# Exercise 4: append "11" to the end of the original array - Prepend "0" to the beginning 
+ 
+a.push(11)
+a.unshift(0)
+p a
+
+# Exercise 5: Get rid of "11" and append "3"
+
+a.delete_at(11)
+a.push(3)
+p a
+
+# Exercise 6: Get of duplicates...
+
+new_arr = a.uniq
+    p new_arr
+
+# Exercise 7: major differences between Arrays and Hashes?
+    #Arrays are collections of ordered, indexed list of any object
+    #Hashes are collections of key value pairs
+    
+# Exercise 8: create a hash using both syntax style
+h1 = {:name => 'Jane', :age => '25 year-old', :city => 'Ponce, PR'}
+h2 = { name: 'Jane', age: '25 year-old', city: 'Ponce, PR'}
+p h1
+    p h2
+    
+# Exercise 9: get value, add to..., remove,
+h = {a:1, b:2, c:3, d:4}
+
+    
+    
+    
+    
+
---
+++ More_Stuff.rb
@@ -0,0 +1,25 @@
+# Exercise 1 - Chapter: More Stuff
+
+def has_lab(string)
+  if string =~ /lab/
+    puts "#{string}"
+  else 
+    puts "No Match"
+  end
+end
+
+has_lab("laboratory")
+has_lab("experiment")
+has_lab("Pans Labyrinth")
+has_lab("elaborate")
+has_lab("polar bear")
+
+# Exercise 2 & 4 - Chapter: More Stuff
+
+def execute(&block)
+  block.call 
+end
+
+execute do
+  puts "Hello from inside the execute method!" 
+end 
