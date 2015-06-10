90d7ca402bb9438436b91aade05d5b8e66f1197f
gabopr <gisantiago@live.com>
2015-06-10 01:51:01 +0000

    exercises completed

--- End_Of_Book.rb
+++ End_Of_Book.rb
@@ -72,3 +72,85 @@ p hash
  
  p arr
 
+# Exercise 11:
+
+
+# Exercise 12: Arrya into the empty hash...
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+
+
+contacts['Joe Smith'][:email] = contact_data[0][0] 
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
+
+puts contacts
+
+# Exercise 13: access Joe's email and Sally's phone number
+puts "Joe's email: #{contacts["Joe Smith"][:email]}"
+puts " Sally's phone number: #{contacts["Sally Johnson"][:phone]}"
+
+# Exercise 14: loop or iterate over "contacts"
+contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each do |name, hash|
+  fields.each do |fields|
+    hash[fields] = contact_data.shift
+  end
+end
+ puts contacts
+
+
+# Exercise 15: Array Method "delete_if" and String Method "start_with?"...
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+#delete all the words that start with an "s" from the array above
+
+arr.delete_if {|x| x.start_with?("s")}
+arr.delete_if {|x| x.start_with?("s", "w")}
+
+puts arr
+
+#Exercise 16: 
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+     
+a.map! {|words| words.split}.flatten!
+p a
+
+#Exercise 17: What will the following program output? "These hashes are not..."
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
